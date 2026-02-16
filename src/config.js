0746ce58dfc704b316a6c78f62fc04a9ca47008f
Martina <iowoi.ouo@gmail.com>
2026-02-17 00:44:53 +0800

    update data

--- src/config.js
+++ src/config.js
@@ -5,7 +5,7 @@
 const config = {
   // JSONBin 設定
   API_KEY: '$2a$10$GeeaO.HWzOMwBMOrF3XkceZdVwaMcJgr.sqx.pCWF/sSR2VfzFZrq',
-  BIN_ID: import.meta.env.VITE_JSONBIN_BIN_ID || '6981c248d0ea881f409d3788',
+  BIN_ID: import.meta.env.VITE_JSONBIN_BIN_ID || '6993496043b1c97be983d918',
 
   // ImgBB 圖片上傳設定
   IMGBB_API_KEY: '5cbce8288a96071b5e9d505cbdd69846',
